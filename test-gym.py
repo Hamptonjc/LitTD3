05099c6791ad9d86f09bb89247810c97afb590dc
Jonathan Hampton <jonathan.hampton@radiancetech.com>
2022-01-05 03:40:05 +0000

    make environment agnostic

--- test-gym.py
+++ test-gym.py
@@ -8,11 +8,16 @@ policy.eval()
 
 env = gym.make(config.GYM_ENVIRONMENT)
 state = env.reset()
+for h, l in zip(env.action_space.high, env.action_space.low):
+    print(h, l)
 
 with torch.no_grad():
     while True:
         env.render()
-        state = torch.tensor(state.copy(), dtype=torch.float32).permute(2,0,1).unsqueeze(0)
+        if len(env.observation_space.shape) > 1:
+            state = torch.tensor(state.copy(), dtype=torch.float32).permute(2,0,1).unsqueeze(0)
+        else:
+            state = torch.tensor(state.copy(), dtype=torch.float32).unsqueeze(0)
         action = policy(state)
         action = action.squeeze(0).numpy()
         state, reward, done, info = env.step(action)
